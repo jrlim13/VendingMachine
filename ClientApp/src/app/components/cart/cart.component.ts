a97dd082c9bf8838fae22189041a185599e85352
jrlim13 <jrlim113@gmail.com>
2022-04-21 11:42:23 +1000

    latest

--- ClientApp/src/app/components/cart/cart.component.ts
+++ ClientApp/src/app/components/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, SimpleChanges, OnChanges, EventEmitter } from '@angular/core';
-import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-cart',
